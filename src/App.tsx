5ca6ac34cfadfdcd6b3755fe7243b4a877360294
e6ntff <pashatar2@gmail.com>
2024-01-02 19:11:06 +0300

    added words logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import TextField from './components/TextField';
 import Options from './components/Options';
@@ -6,6 +6,27 @@ import Options from './components/Options';
 const App: React.FC = () => {
 	const [initialSeconds, setInitialSeconds] = useState<number>(60);
 	const [seconds, setSeconds] = useState<number>(initialSeconds);
+	const [rightWords, setRightWords] = useState<number>(0);
+	const [isStarted, setIsStarted] = useState<boolean>(false);
+	const [accuracy, setAccuracy] = useState<number>(1);
+
+	useEffect(() => {
+		const timerId = isStarted
+			? setInterval(() => {
+					setSeconds((prev: number) => prev - 1);
+			  }, 1000)
+			: '';
+		if (seconds === 0) {
+			clearInterval(timerId);
+		}
+		return () => {
+			clearInterval(timerId);
+		};
+	}, [isStarted, seconds]);
+
+	useEffect(() => {
+		setSeconds(initialSeconds);
+	}, [initialSeconds]);
 
 	return (
 		<>
@@ -13,9 +34,17 @@ const App: React.FC = () => {
 				<Header />
 				<Options
 					seconds={seconds}
+					initialSeconds={initialSeconds}
 					setInitialSeconds={setInitialSeconds}
+					rightWords={rightWords}
+					accuracy={accuracy}
 				/>
-				<TextField />
+				<TextField
+					rightWords={rightWords}
+					setRightWords={setRightWords}
+					setIsStarted={setIsStarted}
+					setAccuracy={setAccuracy}
+				/>
 			</div>
 		</>
 	);
