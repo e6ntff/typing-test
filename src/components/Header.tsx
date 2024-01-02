a0a607c10e8a5eb93ecf137ca17b3bd4f9049bd0
e6ntff <pashatar2@gmail.com>
2024-01-02 14:23:21 +0300

    added header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+	return (
+		<div className=''>
+			<h1 className='font-montserrat text-yellow-700 font-bold text-7xl'>
+				Test your typing skills
+			</h1>
+		</div>
+	);
+};
+
+export default Header;
