351f7b0dcd8535ffac900ed2a52a327fe40ae4d5
e6ntff <pashatar2@gmail.com>
2024-01-02 14:23:31 +0300

    added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
+import TextField from './components/TextField';
 
-const App:React.FC = () => {
-  return <div></div>
-}
+const App: React.FC = () => {
+	return (
+		<>
+			<div className='hidden lg:flex flex-col justify-center items-center gap-10 w-full h-full bg-yellow-100'>
+				<Header />
+				<TextField />
+			</div>
+		</>
+	);
+};
 
 export default App;
