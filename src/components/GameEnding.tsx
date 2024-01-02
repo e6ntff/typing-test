cfc1da0bb200b53a56e81a3f3f23dd487246754f
e6ntff <pashatar2@gmail.com>
2024-01-02 21:45:53 +0300

    fixed maths

--- src/components/GameEnding.tsx
+++ src/components/GameEnding.tsx
@@ -14,11 +14,14 @@ const GameEnding: React.FC<Props> = ({
 	return (
 		<div className='flex flex-col gap-5 items-center'>
 			<span className='text-3xl'>
-				You're typing {rightWords} words per minute with accuracy{' '}
-				{Math.round(accuracy * (60 / initialSeconds) * 100)}%
+				You're typing {rightWords * (60 / initialSeconds)} words per minute with
+				accuracy {Math.round(accuracy * 100)}%
 			</span>
-			<button className='w-32 h-16 bg-yellow-500 text-xl flex justify-center items-center
-				 uppercase font-montserrat font-semibold rounded-lg'>
+			<button
+				className='w-32 h-16 bg-yellow-500 text-xl flex justify-center items-center
+				 uppercase font-montserrat font-semibold rounded-lg'
+				onClick={() => window.location.reload()}
+			>
 				reload&nbsp;&#8634;
 			</button>
 		</div>
