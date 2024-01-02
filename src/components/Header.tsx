53c2b515276f30603cfff79ba863070213d9e6d1
e6ntff <pashatar2@gmail.com>
2024-01-02 19:10:54 +0300

    changed header color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import TimeScreen from './TimeScreen';
-import TimeSelect from './TimeSelect';
 
 const Header: React.FC = () => {
 	return (
-		<h1 className='font-montserrat text-yellow-700 font-bold text-7xl'>
+		<h1 className='font-montserrat font-bold text-7xl'>
 			Test your typing skills
 		</h1>
 	);
