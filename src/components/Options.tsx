294de58fe39eebcb6aa2958df02c382d117a6675
e6ntff <pashatar2@gmail.com>
2024-01-02 19:11:17 +0300

    added options

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
 import TimeScreen from './TimeScreen';
 import TimeSelect from './TimeSelect';
+import WordsScreen from './WordsScreen';
 
 interface Props {
 	seconds: number;
+	initialSeconds: number;
 	setInitialSeconds: (arg0: number) => void;
+	rightWords: number;
+	accuracy: number;
 }
 
-const Options: React.FC<Props> = ({ seconds, setInitialSeconds }) => {
+const Options: React.FC<Props> = ({
+	seconds,
+	initialSeconds,
+	setInitialSeconds,
+	rightWords,
+	accuracy,
+}) => {
 	return (
-		<div className='w-1/2 h-10'>
-			<ul className='w-full h-full flex justify-between'>
+		<div className='w-3/5 h-14'>
+			<ul className='w-full h-full flex justify-between items-center'>
+				<li>
+					<TimeSelect
+						initialSeconds={initialSeconds}
+						setInitialSeconds={setInitialSeconds}
+					/>
+				</li>
+				<li>
+					<WordsScreen
+						rightWords={rightWords}
+						accuracy={accuracy}
+					/>
+				</li>
 				<li>
 					<TimeScreen seconds={seconds} />
 				</li>
-				<li>
-					<TimeSelect />
-				</li>
-				<li></li>
 			</ul>
 		</div>
 	);
