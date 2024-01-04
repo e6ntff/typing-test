5e71420522fc61bd585a19f09ee55db6db085d3d
e6ntff <pashatar2@gmail.com>
2024-01-04 18:50:57 +0300

    moved to daisyUI themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        montserrat: ['Montserrat', 'sans-serif'],
-      },
-      colors: {
-        bg: 'var(--bg-color)',
-        main: 'var(--main-color)',
-        accent: 'var(--accent-color)',
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				montserrat: ['Montserrat', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: ['light', 'dark', 'retro', 'cyberpunk', 'synthwave'],
+	},
 };
