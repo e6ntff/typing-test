9057796adb7e295157e20de78aee2b58fd3cc432
e6ntff <pashatar2@gmail.com>
2024-01-04 18:58:58 +0300

    fixed no mobile style

--- src/components/NoMobile.tsx
+++ src/components/NoMobile.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NoMobile: React.FC = () => {
 	return (
-		<div className='flex lg:hidden w-full h-full bg-yellow-100 justify-center items-center'>
+		<div className='flex lg:hidden w-full h-full justify-center items-center'>
 			<h1 className='text-2xl font-montserrat text-center text-accent'>Sorry, this app is unavailable on mobile devices</h1>
 		</div>
 	);
