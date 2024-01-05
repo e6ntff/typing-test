4b42f5402ab0a0f3503e712be36dd16daafdecf2
e6ntff <pashatar2@gmail.com>
2024-01-06 00:14:19 +0300

    maked custom select

--- src/components/TimeSelect.tsx
+++ src/components/TimeSelect.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
 	initialSeconds: number;
@@ -6,39 +6,58 @@ interface Props {
 }
 
 const TimeSelect: React.FC<Props> = ({ initialSeconds, setInitialSeconds }) => {
-	const setSeconds = (event: React.ChangeEvent<HTMLSelectElement>) => {
-		const seconds = parseInt(event.target.value);
+	const [isExpanded, setIsExpanded] = useState<boolean>(false);
+	const setSeconds = (index: number) => {
+		const seconds = parseInt(data.options[index]);
 		setInitialSeconds(seconds);
 		localStorage.setItem('initSec', String(seconds));
+		toggleList();
+	};
+
+	const toggleList = () => {
+		setIsExpanded((prevValue: boolean) => !prevValue);
+	};
+
+	const data = {
+		value: initialSeconds,
+		options: ['60s', '30s', '15s'],
 	};
 
 	return (
-		<div className='h-full w-28 flex items-center text-3xl font-semibold text-accent'>
+		<div className='h-full flex items-center text-3xl font-semibold text-accent bg-transparent'>
 			<span>🕑</span>
-			<select
-				value={initialSeconds.toString()}
-				onChange={setSeconds}
-				className='w-full h-full bg-transparent rounded-xl border-2 border-solid border-accent'
+			<div
+				className={`w-24 h-full rounded-xl border-4 border-solid border-accent relative bg-transparent transition-all
+			${isExpanded && 'rounded-b-none border-b-0 border-b-transparent'}`}
 			>
-				<option
-					className='font-semibold'
-					value='60'
+				<button
+					onClick={toggleList}
+					className='w-full h-full flex justify-between px-1'
 				>
-					60s
-				</option>
-				<option
-					className='font-semibold'
-					value='30'
-				>
-					30s
-				</option>
-				<option
-					className='font-semibold'
-					value='15'
-				>
-					15s
-				</option>
-			</select>
+					<span>{data.value}s</span>
+					<span className={`${!isExpanded && 'rotate-90'} transition-all`}>
+						˅
+					</span>
+				</button>
+				<ul className='h-full w-24 -ms-1 absolute left-0 top-full transition-all'>
+					{data.options.map((option: string, index: number) => (
+						<li
+							key={index}
+							className={`${
+								!isExpanded ? 'h-0' : ' border-x-4 last:border-4'
+							} h-full overflow-hidden transition-all border-accent px-1 last:border-t-0 last:rounded-b-xl last:box-content last:pb-1`}
+						>
+							<button
+								className='relative transition-all 
+								before:w-0 before:absolute before:left-0 before:bottom-0 before:h-0.5 before:bg-accent hover:before:w-full before:transition-all '
+								onClick={() => setSeconds(index)}
+							>
+								{option}
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
