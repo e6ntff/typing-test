e8af82576d5060b9395d7aa1846b74e1f9bb5d66
e6ntff <pashatar2@gmail.com>
2024-01-05 15:09:13 +0300

    added error handling

--- src/utils/getWords.ts
+++ src/utils/getWords.ts
@@ -1,13 +1,22 @@
 import axios from 'axios';
 
 const getWords = async () => {
-	return await axios
-		.get('https://random-word-api.herokuapp.com/word?number=1000')
-		.then((data) => ({
+	try {
+		return await axios
+			.get('https://random-word-api.herokuapp.com/word?number=1000')
+			.then((data) => ({
+				prev: [],
+				current: data.data[0],
+				next: data.data.slice(1),
+			}));
+	} catch (error) {
+		alert(error);
+		return {
 			prev: [],
-			current: data.data[0],
-			next: data.data.slice(1),
-		}));
+			current: '',
+			next: [],
+		};
+	}
 };
 
 export default getWords;
