7d28a0380cda74aac45f0fe7e3c77ad0cf55f438
e6ntff <pashatar2@gmail.com>
2024-01-07 00:59:39 +0300

    added overlay checkbox

---
+++ src/components/OverlayCheckbox.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface Props {
+	isKeyboard: boolean;
+	setIsKeyboard: (arg0: (arg0: boolean) => boolean) => void;
+}
+
+const OverlayCheckbox: React.FC<Props> = ({ isKeyboard, setIsKeyboard }) => {
+	return (
+		<div
+			className='w-32 h-14 text-4xl flex justify-center items-center select-none uppercase 
+				font-montserrat font-semibold cursor-pointer border-4 border-accent rounded-xl'
+			onClick={() => {
+				setIsKeyboard((prev: boolean) => !prev);
+			}}
+		>
+			<span className='relative before:rounded-xl before:w-0 before:absolute before:left-0 before:bottom-0 before:h-1 before:bg-accent hover:before:w-full before:transition-all'>
+				{!isKeyboard ? 'abc' : 'keys'}
+			</span>
+		</div>
+	);
+};
+
+export default OverlayCheckbox;
