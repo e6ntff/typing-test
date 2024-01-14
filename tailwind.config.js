e251d8f6943e75a49b52fc44904ee78ed0305380
e6ntff <pashatar2@gmail.com>
2024-01-14 20:23:10 +0300

    changed config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,13 @@ module.exports = {
 			},
 			width: {
 				30: '7.5rem',
+				backspacemin: '4.25rem',
 				space: '41.5rem',
+				spacemin: '11rem',
+				menu: '23.25rem',
+				58: '14.5rem',
 			},
-			height: { 30: '7.5rem' },
+			height: { 30: '7.5rem', 62: '15.5rem' },
 			keyframes: {
 				swim: {
 					'0%, 100%': { marginLeft: '0' },
